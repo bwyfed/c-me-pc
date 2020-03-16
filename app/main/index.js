78db185c994b877fe72c03e6c041c56df25ae317
Bianwy <bianwy025@qq.com>
2020-03-16 19:09:16 +0800

    '封装了生成窗口的逻辑，优化了窗口的显示逻辑，使用封装好的类生成指定的窗口，并测试加载本地文件的功能。'

--- app/main/index.js
+++ app/main/index.js
@@ -1,19 +1,28 @@
 const { app, BrowserWindow } = require('electron');
 const isDev = require('electron-is-dev');
 const { resolve } = require('path');
+const AppWindow = require('./common/AppWindow');
 
-let win;
+let mainWindow;
 app.on('ready', () => {
-  win = new BrowserWindow({
-    width: 1080,
-    height: 568,
-    webPreferences: {
-      nodeIntegration: true
-    }
+  const mainWindowConfig = {
+    width: 1440,
+    height: 768
+  };
+  const urlLocation = isDev
+    ? 'http://localhost:8080/'
+    : `file://${resolve(__dirname, '../renderer/pages/main/index.html')}`;
+  // const urlTest = `file://${resolve(
+  //   __dirname,
+  //   '../renderer/pages/main/index.html'
+  // )}`;
+  mainWindow = new AppWindow(mainWindowConfig, urlLocation);
+  mainWindow.on('closed', () => {
+    mainWindow = null; // 回收变量
   });
-  if (isDev) {
-    win.loadURL('http://localhost:8080/');
-  } else {
-    win.loadFile(resolve(__dirname, '../renderer/pages/main/index.html'));
-  }
+  // if (isDev) {
+  //   win.loadURL('http://localhost:8080/');
+  // } else {
+  //   win.loadFile(resolve(__dirname, '../renderer/pages/main/index.html'));
+  // }
 });
