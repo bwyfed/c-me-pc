3056f2f513a3e18e2c3877402210ecc7b398f29f
Bianwy <bianwy025@qq.com>
2020-03-17 19:57:16 +0800

    '增加窗口关闭时app退出的处理；增加构建脚本。'

--- app/main/index.js
+++ app/main/index.js
@@ -2,6 +2,13 @@ const { app } = require('electron');
 const { create: createMainWindow } = require('./windows/main');
 const { create: createLoginWindow } = require('./windows/login');
 
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
 app.on('ready', () => {
   createMainWindow();
 });
