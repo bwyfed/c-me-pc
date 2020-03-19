68299084bb45cd8a4cc569924e057f3be8d28c51
Bianwy <bianwy025@qq.com>
2020-03-19 08:44:14 +0800

    '实现了主进程相关逻辑的代码迁移处理。'

--- app/main/common/AppWindow.js
+++ app/main/common/AppWindow.js
@@ -1,8 +1,7 @@
-import { BrowserWindow } from 'electron';
-import { createProtocol } from 'vue-cli-plugin-electron-builder/lib';
+const { BrowserWindow } = require('electron');
 
 class AppWindow extends BrowserWindow {
-  constructor(config, devPath, prodPath, route) {
+  constructor(config, urlLocation) {
     const baseConfig = {
       width: 800,
       height: 600,
@@ -14,32 +13,11 @@ class AppWindow extends BrowserWindow {
     };
     const finalConfig = { ...baseConfig, ...config };
     super(finalConfig);
-    this.devPath = devPath;
-    this.prodPath = prodPath;
-    this.route = route;
-    if (process.env.WEBPACK_DEV_SERVER_URL) {
-      // Load the url of the dev server if in development mode
-      const devUrl =
-        process.env.WEBPACK_DEV_SERVER_URL + this.devPath + this.route;
-      console.log('devUrl', devUrl);
-      this.loadURL(devUrl);
-      if (!process.env.IS_TEST) this.webContents.openDevTools();
-    } else {
-      if (!AppWindow.createAppProtocol) {
-        createProtocol('cme');
-        AppWindow.createAppProtocol = true;
-      }
-      // Load the index.html when not in development
-      const prodUrl = `app://./${prodPath}${route}`;
-      console.log('prodUrl', prodUrl);
-      this.loadURL(prodUrl);
-    }
+    this.loadURL(urlLocation);
     this.once('ready-to-show', () => {
       this.show();
     });
   }
 }
 
-AppWindow.createAppProtocol = false;
-
-export default AppWindow;
+module.exports = AppWindow;
--- app/main/index.js
+++ app/main/index.js
@@ -1,61 +1,126 @@
-const { app, protocol } = require('electron');
+// 主进程入口
+'use strict';
+
+import { app, protocol, BrowserWindow } from 'electron';
 import {
   createProtocol
-  // installVueDevtools
+  /* installVueDevtools */
 } from 'vue-cli-plugin-electron-builder/lib';
-const { create: createMainWindow } = require('./windows/main');
-const { create: createLoginWindow } = require('./windows/login');
-
 const isDevelopment = process.env.NODE_ENV !== 'production';
+console.log('main process....');
+console.log(isDevelopment);
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let win;
+let secondWin;
+let createAppProtocol = false;
 
-let mainWindow;
-let loginWindow;
-
+// Scheme must be registered before the app is ready
 protocol.registerSchemesAsPrivileged([
-  { scheme: 'cme', privileges: { secure: true, standard: true } }
+  { scheme: 'app', privileges: { secure: true, standard: true } }
 ]);
+
+function createWindow(winVar, winConfig, devPath, prodPath, route) {
+  const baseConfig = {
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    },
+    show: false,
+    backgroundColor: 'efefef'
+  };
+  const finalConfig = { ...baseConfig, ...winConfig };
+  // Create the browser window.
+  winVar = new BrowserWindow(finalConfig);
+
+  if (process.env.WEBPACK_DEV_SERVER_URL) {
+    console.log(
+      'process.env.WEBPACK_DEV_SERVER_URL',
+      process.env.WEBPACK_DEV_SERVER_URL
+    );
+    // Load the url of the dev server if in development mode
+    console.log(process.env.WEBPACK_DEV_SERVER_URL + devPath + route);
+    winVar.loadURL(process.env.WEBPACK_DEV_SERVER_URL + devPath + route);
+    if (!process.env.IS_TEST) winVar.webContents.openDevTools();
+  } else {
+    console.log('build.....');
+    if (!createAppProtocol) {
+      createProtocol('app');
+      createAppProtocol = true;
+    }
+    // Load the index.html when not in development
+    winVar.loadURL(`app://./${prodPath}${route}`);
+  }
+  winVar.once('ready-to-show', () => {
+    winVar.show();
+  });
+  winVar.on('closed', () => {
+    winVar = null;
+  });
+}
+
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
     app.quit();
   }
 });
+const mainWindowConfig = {
+  width: 1024,
+  height: 768
+};
+const loginWindowConfig = {
+  width: 800,
+  height: 600
+};
 
-// Quit when all windows are closed.
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', async () => {
+  if (isDevelopment && !process.env.IS_TEST) {
+    // Install Vue Devtools
+    // Devtools extensions are broken in Electron 6.0.0 and greater
+    // See https://github.com/nklayman/vue-cli-plugin-electron-builder/issues/378 for more info
+    // Electron will not launch with Devtools extensions installed on Windows 10 with dark mode
+    // If you are not using Windows 10 dark mode, you may uncomment these lines
+    // In addition, if the linked issue is closed, you can upgrade electron and uncomment these lines
+    try {
+      await installVueDevtools();
+    } catch (e) {
+      console.error('Vue Devtools failed to install:', e.toString());
+    }
   }
+  createWindow(win, mainWindowConfig, '', 'index.html', '#/about');
+  createWindow(
+    secondWin,
+    loginWindowConfig,
+    'subpage',
+    'subpage.html',
+    '#/about2'
+  );
 });
-
 app.on('activate', () => {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    mainWindow = createMainWindow();
+  if (win === null) {
+    createWindow(win, mainWindowConfig, '', 'index.html', '#/about');
   }
-  if (loginWindow === null) {
-    loginWindow = createLoginWindow();
+  if (secondWin === null) {
+    createWindow(
+      secondWin,
+      loginWindowConfig,
+      'subpage',
+      'subpage.html',
+      '#/about2'
+    );
   }
 });
 
-app.on('ready', async () => {
-  // if (isDevelopment && !process.env.IS_TEST) {
-  //   try {
-  //     await installVueDevtools();
-  //   } catch (e) {
-  //     console.error('Vue Devtools failed to install:', e.toString());
-  //   }
-  // }
-  mainWindow = createMainWindow();
-  loginWindow = createLoginWindow();
-  mainWindow.on('closed', () => {
-    mainWindow = null;
-  });
-  loginWindow.on('closed', () => {
-    loginWindow = null;
-  });
-});
+// Exit cleanly on request from parent process in development mode.
 if (isDevelopment) {
   if (process.platform === 'win32') {
     process.on('message', data => {
